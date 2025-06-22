45bacec92fa5358c8c3a224301fc8e991bf10781
VihaanPihu <nitishmbr@gmail.com>
2025-06-22 18:37:23 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,3 +4,4 @@ st.set_page_config(page_title="Main Home", layout="centered")
 
 st.title("📚 Welcome to the Learning App!")
 st.write("Use the sidebar to navigate to different pages.")
+st.image("family.jpeg")
