0ee64ed73f00fbc141026bcfdb88417943c73df9
VihaanPihu <nitishmbr@gmail.com>
2025-06-22 18:40:18 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from pathlib import Path
 
 st.set_page_config(page_title="Main Home", layout="centered")
 
 st.title("📚 Welcome to the Learning App!")
 st.write("Use the sidebar to navigate to different pages.")
-st.image("family.jpeg")
+
+image_path = Path(__file__).parent / "family.jpeg"
+st.image(image_path)
