ad3774b19064f351226364e557e48194cb222e63
VihaanPihu <nitishmbr@gmail.com>
2025-06-22 15:50:08 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Main Home", layout="centered")
+
+st.title("📚 Welcome to the Learning App!")
+st.write("Use the sidebar to navigate to different pages.")
