cdcabe4a2c29f73b940d60338df3996693029768
Nitish Rai <nitishmbr@gmail.com>
2025-07-04 21:03:36 +0530

    Update multiplication.py

--- pages/multiplication.py
+++ pages/multiplication.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import random
 
-st.set_page_config(page_title="Multiplication Practice", page_icon="➕")
+st.set_page_config(page_title="Multiplication Practice", page_icon="X")
 
 st.title("🧒 multiplication Practice for Nitya")
 
